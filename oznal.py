ad91abbfd82f4dba28f758a0a393236054042723
Majky336 <sojka.michal.96@gmail.com>
2019-05-08 12:49:40 +0200

    WIP

--- oznal.py
+++ oznal.py
@@ -13,25 +13,45 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
+from sklearn.feature_selection import RFE
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import precision_score
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.svm import SVR
+from sklearn.utils import check_array
 
-# Util functions
+
+# > Util functions
+
+
+def reject_outliers(sr, iq_range=0.5):
+    pcnt = (1 - iq_range) / 2
+    qlow, median, qhigh = sr.dropna().quantile([pcnt, 0.50, 1-pcnt])
+    iqr = qhigh - qlow
+    return sr[(sr - median).abs() <= iqr]
+
 
 def remove_outlier(df_in, col_name):
     q1 = df_in[col_name].quantile(0.25)
     q3 = df_in[col_name].quantile(0.75)
-    iqr = q3-q1 #Interquartile range
-    fence_low  = q1-1.5*iqr
+    iqr = q3-q1  # Interquartile range
+    fence_low = q1-1.5*iqr
     fence_high = q3+1.5*iqr
-    df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
+    df_out = df_in.loc[(df_in[col_name] > fence_low) &
+                       (df_in[col_name] < fence_high)]
     return df_out
 
-# Import dataset
 
-# Nacitanie datasetu
-data = pd.DataFrame(pd.read_csv('listings.csv'))
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    y_true[y_true == 0] = 1
+
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+# < Util functions
+
 # Vybratie stlpcov z datasetu
 df = pd.DataFrame(data=pd.read_csv('listings.csv'), columns=[
     'price',
@@ -40,46 +60,68 @@ df = pd.DataFrame(data=pd.read_csv('listings.csv'), columns=[
     'number_of_reviews',
     'room_type',
     'neighbourhood'
-    ])
+])
 
-df2 = pd.DataFrame(data=pd.read_csv('listings_details.csv'))
-print(df2.columns.tolist())
+df2 = pd.DataFrame(data=pd.read_csv('listings_details.csv'), columns=[
+    'property_type',
+    'bathrooms',
+    'bedrooms',
+    'beds',
+])
 
-df = remove_outlier(df, 'availability_365')
-df = remove_outlier(df, 'minimum_nights')
-df = remove_outlier(df, 'number_of_reviews')
+# print(df.columns.tolist())
+# print(df2.columns.tolist())
+# print("--------------------------------------------")
+data = df.join(df2)
 
-mapper = { 'Private room': 0, 'Entire home/apt': 1, 'Shared room': 2}
-df = df.replace({ 'room_type': mapper })
-neighbourhoods = df.neighbourhood.unique().tolist()
-neighbour_mapper = {neighbourhoods[i]: i for i in range(0, len(neighbourhoods))}
-df = df.replace({'neighbourhood': neighbour_mapper})
-
-# Vypis tabulku korelacie
-print(df.corr())
-
-pd.plotting.scatter_matrix(df, figsize=(7, 7))
-plt.show()
+features = ['availability_365', 'minimum_nights', 'number_of_reviews',
+            'room_type', 'neighbourhood', 'property_type', 'bathrooms', 'bedrooms', 'beds']
+target = ['price']
 
 
-X = df.iloc[:, [1, 2, 3, 4, 5]].values
-y = df.iloc[:, 0].values
+data = remove_outlier(data, 'availability_365')
+data = remove_outlier(data, 'minimum_nights')
+data = remove_outlier(data, 'number_of_reviews')
+data = remove_outlier(data, 'price')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=9)
+# # Vypis tabulku korelacie
+print(data.corr())
 
-bp = plt.boxplot(X_train)
-plt.show()
+X = data.iloc[:][features]
+y = data.iloc[:][target]
 
-linear_model = LinearRegression(fit_intercept=True)
-linear_model.fit(X_train,y_train)
+X = X.fillna(0)
+
+# X_dict = X.to_dict(orient='record')
+# dv_X = DictVectorizer(sparse=False)
+# X_encoded = dv_X.fit_transform(X_dict)
+
+# get_dummies method
+X = pd.get_dummies(X, prefix_sep='', drop_first=True)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=0.8, test_size=0.2, random_state=9)
+
+# print(X_train)
+# print(X_train.dropna(axis=0, how='any'))
+
+# # bp = plt.boxplot(X_train)
+# # plt.show()
+
+linear_model = LinearRegression()
+linear_model.fit(X_train, y_train)
 
 predictions = linear_model.predict(X_test)
-plt.scatter(y_test,predictions)
+plt.scatter(y_test, predictions, color='blue')
 plt.xlabel("True Values")
 plt.ylabel("Predictions")
 
 print(linear_model.score(X_test, y_test))
 print("Mean squared error:", mean_squared_error(y_test, predictions))
+print("Mean average percentage error:",
+      mean_absolute_percentage_error(y_test, predictions))
+print('Variance score: %.2f' % r2_score(y_test, predictions))
 plt.show()
 
 # print('K fold')
